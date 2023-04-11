b8e70e862dd257e6930a24f184850de914d5feb9
Jesus Romo <romojesus03@gmail.com>
2023-04-11 15:21:55 -0700

    House and Documenation

---
+++ house.py
@@ -0,0 +1,19 @@
+def draw_house(x, y, width, height): 
+
+    """
+    This function draws house with width and height from the reference point
+
+    param x: x cooridnate of the midpoint of the foundation 
+    param y: y cooridnate of the bottom side of the foundation 
+    param width: total width of the house 
+    param height: total height of the house 
+    
+    """
+    print("Drawing house", x, y, width, height)
+    pass
+
+
+x, y = 100, 100
+width, height = 100, 400 
+
+draw_house(x, y , width, height)
