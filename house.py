4fe1c4db01926aa9fcc27df40fbb9932be5e6d2b
Jesus Romo <romojesus03@gmail.com>
2023-04-13 09:19:36 -0700

    Added Documentation

--- house.py
+++ house.py
@@ -1,3 +1,12 @@
+def main():
+
+    x, y = 100, 100
+    width, height = 100, 400 
+
+    draw_house(x, y , width, height)
+
+
+
 def draw_house(x, y, width, height): 
 
     """
@@ -10,10 +19,55 @@ def draw_house(x, y, width, height):
     
     """
     print("Drawing house", x, y, width, height)
+    
+    foundation_height = 0.05 * height 
+    walls_width = 0.9 * width 
+    walls_height = 0.5 * height
+    roof_height = height - foundation_height - walls_height
+
+
+    draw_house_foundation(x, y, width, foundation_height)
+    draw_house_walls(x, y - foundation_height, walls_width, walls_height)
+    draw_house_roof(x, y - foundation_height - walls_height, width, roof_height)
+
+
+def draw_house_foundation(x, y, width, height):
+    """
+    function draws house foundation with width and height from reference point
+    
+    param x: x coordinate of the midpoint of the foundation
+    param y: y coordinate of the bottom side of the foundation
+    param width: total width of the foundation
+    param height: total height of the foundation
+
+    """
+
+    print("Drawing foundation", x, y, width, height)
+    pass
+def draw_house_walls(x, y,  width, height):
+    """
+    function draws house walls with width and height from reference point
+    
+    param x: x coordinate of the midpoint of the foundation
+    param y: y coordinate of the bottom side of the foundation
+    param width: total width of the walls
+    param height: total height of the walls
+    
+
+    """
+    print("Drawing walls ", x, y, width, height)
+    pass
+def draw_house_roof(x, y, width, height):
+    """
+    function draws house roof with width and height from reference point
+    
+    param x: x coordinate of the midpoint of the foundation
+    param y: y coordinate of the bottom side of the foundation
+    param width: total width of the roof
+    param height: total height of the roof
+    
+    """
+    print("Drawing roof", x, y, width, height)
     pass
 
-
-x, y = 100, 100
-width, height = 100, 400 
-
-draw_house(x, y , width, height)
+main()
